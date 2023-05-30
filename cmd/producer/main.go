f99fe7a7c4e94f4ef2d2902ce56237368dffa4f3
enchik0reo <profipingvin@gmail.com>
2023-05-30 11:10:36 +0700

    add fake orders

--- cmd/producer/main.go
+++ cmd/producer/main.go
@@ -2,13 +2,13 @@ package main
 
 import (
 	"encoding/json"
-	"io"
 	"log"
-	"os"
 	"strconv"
+	"time"
 
 	"github.com/enchik0reo/wildberriesL0/internal/models"
 
+	"github.com/brianvoe/gofakeit/v6"
 	"github.com/nats-io/stan.go"
 )
 
@@ -25,34 +25,81 @@ func main() {
 	}
 	defer sc.Close()
 
-	file, err := os.Open("internal/models/model.json")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer file.Close()
-
-	bytes, err := io.ReadAll(file)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	model := models.Basic{}
-
-	if err = json.Unmarshal(bytes, &model); err != nil {
-		log.Fatal(err)
-	}
-
 	for i := 1; i <= 100; i++ {
-		if err = sc.Publish("orders", newOrder(i, &model)); err != nil {
+		if err = sc.Publish("orders", newOrder(i)); err != nil {
 			log.Fatal(err)
 		}
 
 		log.Printf("order №%d published\n", i)
+
+		time.Sleep(time.Duration(gofakeit.Number(1, 1000)) * time.Millisecond)
 	}
 }
 
-func newOrder(num int, model *models.Basic) []byte {
+func newOrder(num int) []byte {
+	gf := gofakeit.New(0)
+	model := models.Basic{}
+
 	model.OrderUid = "order" + strconv.Itoa(num)
+	model.TrackNumber = "WBILMTESTTRACK"
+	model.Entry = "WBIL"
+	model.Delivery.Name = gf.Name()
+	model.Delivery.Phone = gf.Phone()
+	model.Delivery.Zip = strconv.Itoa(gf.Number(0, 9999999))
+
+	adr := gf.Address()
+
+	model.Delivery.City = adr.City
+	model.Delivery.Address = adr.Street
+	model.Delivery.Region = adr.State
+	model.Delivery.Email = gf.Email()
+
+	for i := 0; i < gf.Number(1, 10); i++ {
+		item := models.Items{}
+
+		item.ChrtId = gf.Number(0, 9999999)
+		item.TrackNumber = "WBILMTESTTRACK"
+		item.Price = gf.Number(1, 10000)
+		item.Rid = gf.UUID()
+		item.Name = "toy car"
+		item.Sale = gf.Number(0, 99)
+		item.Size = "0"
+		item.TotalPrice = item.Price - ((item.Price * item.Sale) / 100)
+		item.NmId = gf.Number(0, 9999999)
+		item.Brand = gf.Car().Brand
+		item.Status = 202
+
+		model.Items = append(model.Items, item)
+	}
+
+	model.Payment.Transaction = gf.UUID()
+	model.Payment.RequestId = ""
+	model.Payment.Currency = gf.CurrencyShort()
+	model.Payment.Provider = "wbpay"
+	model.Payment.PaymentDt = gf.Number(0, 9999999999)
+	model.Payment.Bank = "alpha"
+	model.Payment.DeliveryCost = gf.Number(0, 5000)
+
+	model.Locale = adr.State
+	model.InternalSignature = ""
+	model.CustomerId = "test"
+	model.DeliveryService = "meets"
+	model.Shardkey = "9"
+	model.SmId = gf.Number(0, 1000)
+	model.DateCreated = time.Date(time.Now().Year(), time.Now().Month(), time.Now().Day(), time.Now().Hour(), time.Now().Minute(), time.Now().Second(), time.Now().Nanosecond(), time.UTC)
+	model.OofShard = "1"
+
+	sumPrice := func(c []models.Items) int {
+		s := 0
+		for _, i := range c {
+			s += i.TotalPrice
+		}
+		return s
+	}
+
+	model.Payment.Amount = sumPrice(model.Items) + model.Payment.DeliveryCost
+	model.Payment.GoodsTotal = gf.Number(0, 1000)
+	model.Payment.CustomFee = 0
 
 	bytes, err := json.MarshalIndent(model, "", " ")
 	if err != nil {
--- go.mod
+++ go.mod
@@ -14,6 +14,7 @@ require (
 
 require (
 	github.com/BurntSushi/toml v1.1.0 // indirect
+	github.com/brianvoe/gofakeit/v6 v6.21.0 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
